9b818201986202e59de5aab115961858211c6fb2
MilkTwoSugars <amackey.mail@gmail.com>
2018-05-03 18:16:59 +0100

    colours

--- projects.js
+++ projects.js
@@ -4,9 +4,9 @@ function getProjects() {
     let projects = [];
 
     projects[0] = new Project("#EA4335", GITROOT + "SimpleSynth/", "Simple Synth ", "A basic synthesiser with sliders (requires MIDI keyboard)");
-    projects[1] = new Project("#EA4335", GITROOT + "OceanSynth/", "Ocean Piano", "Bubbles (Requires MIDI keyboard)");
-    projects[2] = new Project("#EA4335", GITROOT + "TouchLFO/", "Touch LFO", "Annoying noise generator");
-    projects[3] = new Project("#EA4335", GITROOT + "Conveyor/", "Conveyor", "Colour mixing");
+    projects[1] = new Project("#99ccff", GITROOT + "OceanSynth/", "Ocean Piano", "Bubbles (Requires MIDI keyboard)");
+    projects[2] = new Project("#00cc99", GITROOT + "TouchLFO/", "Touch LFO", "Annoying noise generator");
+    projects[3] = new Project("#ff8000", GITROOT + "Conveyor/", "Conveyor", "Colour mixing");
 
     return projects;
 }
