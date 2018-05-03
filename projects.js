a1874f543412a9e25e2360fe810c48df5659a8b4
MilkTwoSugars <amackey.mail@gmail.com>
2018-05-03 12:41:46 +0100

    Conveyor

--- projects.js
+++ projects.js
@@ -6,6 +6,7 @@ function getProjects() {
     projects[0] = new Project("#EA4335", GITROOT + "SimpleSynth/", "Simple Synth ", "A basic synthesiser with sliders (requires MIDI keyboard)");
     projects[1] = new Project("#EA4335", GITROOT + "OceanSynth/", "Ocean Piano", "Bubbles (Requires MIDI keyboard)");
     projects[2] = new Project("#EA4335", GITROOT + "TouchLFO/", "Touch LFO", "Annoying noise generator");
+    projects[3] = new Project("#EA4335", GITROOT + "Conveyor/", "Conveyor", "Colour mixing");
 
     return projects;
 }
